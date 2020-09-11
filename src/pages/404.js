f352f26e48546ee2613ece4b810cf4395dae7775
Jiri Stepanek <jiristepanek@MacBook-Pro-6.local>
2020-09-11 23:47:13 +0200

    build

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { navigate } from 'gatsby'
+import { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
-const NotFound = () => {
-  navigate('/');
-  return (<p>404 - sorry jako</p>);
-}
-
-export default NotFound;
+export default () => {
+  useEffect(() => {
+    navigate('/');
+  }, []);
+  return null;
+};
