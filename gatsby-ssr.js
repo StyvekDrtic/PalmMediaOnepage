be4293b0fb0ecc76d1f96f08f0b387a831365936
styvekdrtic <jiristepanek@hotmail.com>
2024-10-31 18:52:43 +0100

    redirect

---
+++ gatsby-ssr.js
@@ -0,0 +1,9 @@
+export const onRenderBody = () => {
+  const meta = [
+    {
+      httpEquiv: 'refresh',
+      content: '0;url=https://www.linkedin.com/in/jirkastepanek/',
+    },
+  ]
+  return { meta }
+}
